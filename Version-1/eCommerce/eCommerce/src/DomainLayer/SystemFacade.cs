11086643d220172e7046171dbee1a477e3dced65
yazan <yazan.ahmad.abass@gmail.com>
2022-04-16 21:40:48 +0300

    fixed herarichy

---
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/SystemFacade.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.src.DomainLayer.User;
+using eCommerce.src.DomainLayer.Store;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.DomainLayer
+{
+    internal interface ISystemFacade
+    {
+
+    }
+
+    public class SystemFacade : ISystemFacade
+    {
+        private UserFacade userFacade;
+        private StoreFacade storeFacade;
+        public SystemFacade()
+        {
+            userFacade = new UserFacade();
+            storeFacade = new StoreFacade();
+        }
+    }
+}
---
+++ Version-1/eCommerce/eCommerce/src/ServiceLayer/Objects/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.ServiceLayer.Objects
+{
+    internal class User
+    {
+        /*DATA ONLY WITHOUT LOGIC*/
+    }
+}
