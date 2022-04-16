9eb8e0dff2bf1eafe7b66f1f98f137eb1f1a7d7a
Tariq Ganem <tariqg@post.bgu.ac.il>
2022-04-16 14:25:29 +0300

    Done with gitignore

--- Version-1/eCommerce/eCommerce/Program.cs
+++ Version-1/eCommerce/eCommerce/Program.cs
@@ -6,7 +6,7 @@ namespace eCommerce
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test for gitignore");
         }
     }
 }
