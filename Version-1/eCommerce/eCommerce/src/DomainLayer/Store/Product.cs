63f9d24a15a0c857ee5110a60751a354e3486b59
eran <mando@post.bgu.ac.il>
2022-04-16 20:30:49 +0300

    store package implementation first commit

---
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/Store/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Concurrent;
+
+namespace eCommerce.src.DomainLayer.Store
+{
+    public class Product
+    {
+        public String Id { get; }
+        public String Name { get; set; }
+        public double Price { get; set; } 
+        public int Quantity { get; set; }
+        public String Category { get; set; }
+        public Double Rating { get; set; }
+        public int AmountOfRate { get; set; }
+
+        public Product(string id, string name, double price, int quantity, string category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Category = category;
+            Rating = 0;
+            AmountOfRate = 0;
+        }
+    }
+}
