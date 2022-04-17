99e789e66dc7cef6436cc8008990408cc6f10881
yazan <yazan.ahmad.abass@gmail.com>
2022-04-17 15:13:43 +0300

    Implemented ShoppingCart

--- Version-1/eCommerce/eCommerce/src/DomainLayer/User/ShoppingCart.cs
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/User/ShoppingCart.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 
 namespace eCommerce.src.DomainLayer.User
 {
-    public class ShoppingCart
+    internal class ShoppingCart
     {
+        public string Id { get; private set; }
+        public ConcurrentDictionary<String, ShoppingBag> ShoppingBags { get; set; }  // <storeId, shoppingBag>
+        public Double TotalCartPrice { get; set; }
+
+        public ShoppingCart()
+        {
+            //Id = Service.GetId();
+            ShoppingBags = new ConcurrentDictionary<string, ShoppingBag>();
+            TotalCartPrice = 0;
+        }
+
+        public ShoppingCart(ShoppingCart shoppingCart)
+        {
+            Id = shoppingCart.Id;
+            ShoppingBags = shoppingCart.ShoppingBags;
+            TotalCartPrice = shoppingCart.TotalCartPrice;
+        }
+
+        public ShoppingBag GetShoppingBag(string storeId)
+        {
+            if (ShoppingBags.TryGetValue(storeId, out ShoppingBag shoppingBag))
+                return shoppingBag;
+            throw new Exception($"Shopping bag not found for the store id: {storeId}.");
+        }
+
+/*        public Boolean AddShoppingBagToCart(ShoppingBag shoppingBag)
+        {
+            return ShoppingBags.TryAdd(shoppingBag.storeId, shoppingBag);
+        }*/
+
+/*        public Double GetTotalShoppingCartPrice()
+        {
+            Double sum = 0;
+            foreach (ShoppingBag bag in ShoppingBags.Values)
+            {
+                sum = sum + bag.GetTotalPrice();
+            }
+            TotalCartPrice = sum;
+            return sum;
+        }*/
     }
 }
