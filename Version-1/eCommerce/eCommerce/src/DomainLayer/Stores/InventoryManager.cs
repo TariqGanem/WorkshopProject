2232fc41a2027ebba43a0b4493aeaa59fb31c99d
Tariq Ganem <tariqg@post.bgu.ac.il>
2022-04-19 00:13:25 +0300

    updated requirements file & adding functionailty to the Stores

--- Version-1/eCommerce/eCommerce/src/DomainLayer/Stores/InventoryManager.cs
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/Stores/InventoryManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace eCommerce.src.DomainLayer.Store
 {
@@ -32,13 +33,91 @@ namespace eCommerce.src.DomainLayer.Store
             {
                 return toRemove;
             }
-            //else
-            throw new Exception($"Trying to remove non-existed product. (ID {productID})");
+            throw new Exception($"Trying to remove non-existed product. (ID: {productID})");
         }
 
         public Product EditProduct(string productID, IDictionary<String, object> details)
         {
+            /*if (Products.TryGetValue(productID, out Product toEdit))
+            {
+                ObjectDictionaryMapper<Product>.SetPropertyValue(toEdit, details);
+            }
+            //else failed
+            throw new Exception($"Faild to edit product (ID: {productID}) - Product not found");*/
             return null;
+        }
+
+        public List<Product> SearchProduct(IDictionary<String, Object> searchAttributes)
+        {
+            List<Product> searchResults = new List<Product>();
+            foreach (Product product in this.Products.Values)
+            {
+                if (CheckProduct(product, searchAttributes))
+                {
+                    searchResults.Add(product);
+                }
+            }
+            if (searchResults.Count > 0)
+            {
+                return searchResults;
+            }
+            else
+            {
+                throw new Exception($"No item has been found");
+            }
+        }
+
+        internal bool CheckProduct(Product product, IDictionary<String, Object> searchAttributes)
+        {
+            Boolean result = true;
+            ICollection<String> properties = searchAttributes.Keys;
+            foreach (string property in properties)
+            {
+                var value = searchAttributes[property];
+                switch (property.ToLower())
+                {
+                    case "name":
+                        if (!product.Name.ToLower().Contains(((string)value).ToLower())) { result = false; }
+                        break;
+                    case "category":
+                        if (!product.Category.ToLower().Equals(((string)value).ToLower())) { result = false; }
+                        break;
+                    case "lowprice":
+                        if (product.Price < (Double)value) { result = false; }
+                        break;
+                    case "highprice":
+                        if (product.Price > (Double)value) { result = false; }
+                        break;
+                    case "keywords":
+                        bool found = false;
+                        List<string> productKeywords = product.KeyWords.Select(word => word.ToLower()).ToList();
+                        List<string> searchWords = (List<String>)value;
+                        for (int i = 0; i < searchWords.Count && !found; i++)
+                        {
+                            if (productKeywords.Contains(searchWords[i].ToLower()))
+                            {
+                                //One keyword has been found
+                                found = true;
+                            }
+                        }
+                        //No keyword has been found
+                        if (!found)
+                        {
+                            result = false;
+                        }
+                        break;
+                }
+            }
+            return result;
+        }
+
+        public Product GetProduct(String productID)
+        {
+            if (Products.TryGetValue(productID, out Product product))
+            {
+                return product;
+            }
+            throw new Exception($"Product (ID: {productID}) not found");
         }
     }
 }
--- Version-1/eCommerce/eCommerce/src/DomainLayer/Stores/Store.cs
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/Stores/Store.cs
@@ -13,8 +13,7 @@ namespace eCommerce.src.DomainLayer.Store
         public String Name { get; }
         public Boolean IsOpen { get; }
         public StoreOwner Founder { get; }
-        public double Rating { get; }
-        public int AmountOfRates { get; }
+        public InventoryManager InventoryManager { get; }
         public StoreHistory History { get; set; }
         public ConcurrentDictionary<String,StoreOwner> Owners { get; }
         public ConcurrentDictionary<String,StoreManager> Managers { get; }
@@ -27,9 +26,77 @@ namespace eCommerce.src.DomainLayer.Store
             Founder = new StoreOwner();
             Name = name;
             Owners.TryAdd(founder.UserName, Founder);
-            AmountOfRates = 0;
-            Rating = 0;
             History = new StoreHistory();
+        }
+
+        public List<Product> SearchProduct(IDictionary<String, Object> searchAttributes)
+        {
+            return InventoryManager.SearchProduct(searchAttributes);
+        }
+
+        public Product AddNewProduct(String userID, String productName, Double price, int initialQuantity, String category, LinkedList<String> keyWords = null)
+        {
+            return null;
+        }
+
+        public Product RemoveProduct(String userID, String productID)
+        {
+            return null;
+        }
+
+        public Product EditProduct(String userID, String productID, IDictionary<String, Object> details)
+        {
+            return null;
+        }
+
+        public Boolean AddStoreOwner(RegisteredUser futureOwner, string currentlyOwnerID)
+        {
+            return false;
+        }
+
+        public Boolean AddStoreManager(RegisteredUser futureManager, string currentlyOwnerID)
+        {
+            return false;
+        }
+
+        public bool RemoveStoreManager(String removedManagerID, string currentlyOwnerID)
+        {
+            return false;
+        }
+
+        public bool SetPermissions(string managerID, string ownerID, LinkedList<int> permissions)
+        {
+            return false;
+        }
+
+        public StoreHistory GetStorePurchaseHistory(string userID, bool sysAdmin)
+        {
+            return null;
+        }
+
+        public bool RemovePermissions(string managerID, string ownerID, LinkedList<int> permissions)
+        {
+            return false;
+        }
+
+        public Product GetProduct(String productID)
+        {
+            return InventoryManager.GetProduct(productID);
+        }
+
+        // Complete functionality for Store
+
+
+
+
+        private Boolean CheckIfStoreOwner(String userID)
+        {
+            return Owners.ContainsKey(userID);
+        }
+
+        private Boolean CheckIfStoreManager(String userID)
+        {
+            return Managers.ContainsKey(userID);
         }
     }
 }
