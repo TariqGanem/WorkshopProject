16ca5adf2f5cbad6ccd06f9c996ba7a5db1ca1cd
Tariq Ganem <tariqg@post.bgu.ac.il>
2022-04-20 00:20:19 +0300

    Starting with testing, adding dependences and XUnit

---
+++ Version-1/eCommerce/eCommerceTests/Stores/StoreTests.cs
@@ -0,0 +1,16 @@
+﻿using eCommerce.src.DomainLayer.Store;
+using eCommerce.src.DomainLayer.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace eCommerceTests.Stores
+{
+    public class StoreTests
+    {
+        
+    }
+}
