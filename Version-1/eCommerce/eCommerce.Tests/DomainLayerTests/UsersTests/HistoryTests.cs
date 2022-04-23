a2f04b522a8f0573ecf110a8db0fc103ecf56a9b
mas98ahmed <ahmedmas@post.bgu.ac.il>
2022-04-23 03:31:24 +0300

    almost done User's tests

---
+++ Version-1/eCommerce/eCommerce.Tests/DomainLayerTests/UsersTests/HistoryTests.cs
@@ -0,0 +1,45 @@
+﻿using eCommerce.src.DomainLayer.User;
+using NUnit.Framework;
+using eCommerce.src.DomainLayer.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Tests.DomainLayerTests.UsersTests
+{
+    public class HistoryTests
+    {
+        public History history = new History();
+        public ShoppingCart cart = new ShoppingCart();
+        public Store mashbir = new Store("Mashbir", null);
+        public Store shofersal = new Store("Shofersal", null);
+
+        [SetUp]
+        public void Setup()
+        {
+            history.ShoppingBags.AddLast(new ShoppingBag("1", null));
+            cart.AddShoppingBagToCart(new ShoppingBag("2", mashbir));
+            cart.AddShoppingBagToCart(new ShoppingBag("3", shofersal));
+        }
+
+        [Test]
+        public void AddPurchasedShoppingCartTest()
+        {
+            history.AddPurchasedShoppingCart(cart);
+            Assert.That(history.ShoppingBags.Count, Is.EqualTo(3));
+            Assert.IsTrue(history.ShoppingBags.Contains(cart.ShoppingBags[mashbir.Id]));
+            Assert.IsTrue(history.ShoppingBags.Contains(cart.ShoppingBags[shofersal.Id]));
+        }
+
+        [Test]
+        public void AddPurchasedShoppingBagTest()
+        {
+            ShoppingBag shoppingBag = new ShoppingBag("2",null);
+            history.AddPurchasedShoppingBag(shoppingBag);
+            Assert.That(history.ShoppingBags.Count, Is.EqualTo(2));
+            Assert.IsTrue(history.ShoppingBags.Contains(shoppingBag));
+        }
+    }
+}
