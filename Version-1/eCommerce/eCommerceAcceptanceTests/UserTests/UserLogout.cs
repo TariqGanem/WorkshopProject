a1e06f14eb8a9e64ab0cb6f536411f5085dbb518
eran <mando@post.bgu.ac.il>
2022-05-12 03:12:10 +0300

    Writing Acceptance Testing - Working on the user

---
+++ Version-1/eCommerce/eCommerceAcceptanceTests/UserTests/UserLogout.cs
@@ -0,0 +1,42 @@
+﻿using eCommerce.src.ServiceLayer.Objects;
+using eCommerce.src.ServiceLayer.ResultService;
+using eCommerceIntegrationTests.Utils;
+using System;
+using Xunit;
+
+namespace eCommerceAcceptanceTests.UserTests
+{
+    public class UserLogout : XeCommerceTestCase
+    {
+        public UserLogout() : base() { }
+
+        [Fact]
+        [Trait("Category", "AcceptanceTests")]
+        public void HappyLogout()
+        {
+            api.Register("goodtest@gmail.com", "GoodPass");
+            Result<String> userIdRes = api.Login("goodtest@gmail.com", "GoodPass");
+            Result res = this.api.Logout(userIdRes.Value); // logging out by user_id - not email 
+            Assert.True(!res.ErrorOccured);
+        }
+
+        [Fact]
+        [Trait("Category", "AcceptanceTests")]
+        public void sadLogoutTest_notLoginedUser()
+        {
+            api.Register("goodtest@gmail.com", "GoodPass");
+            Result res = this.api.Logout("goodtest@gmail.com");
+            Assert.True(res.ErrorOccured);
+        }
+
+        [Fact]
+        [Trait("Category", "AcceptanceTests")]
+        public void sadLogoutTest_WrongEmail()
+        {
+            api.Register("goodtest@gmail.com", "GoodPass");
+            api.Login("goodtest@gmail.com", "GoodPass");
+            Result res = this.api.Logout("goodtest@gmail.cm");
+            Assert.True(res.ErrorOccured);
+        }
+    }
+}
