d9a658ad2fb270a1771a8efd8e601bc3cdc9f5f5
eran <mando@post.bgu.ac.il>
2022-05-16 23:58:23 +0300

    Process of Impl the Observer for notifications - Subscriber Interface

---
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/Notifications/Observer/SubscriberInterface.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.src.DomainLayer.Notifications;
+using eCommerce.src.ServiceLayer.ResultService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.DomainLayer
+{
+    public interface SubscriberInterface
+    {
+            Result<Boolean> Update(Notification notification);
+    }
+}
