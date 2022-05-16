6e198df298d0f35dd9656ea05ac97ca195f8fff0
eran <mando@post.bgu.ac.il>
2022-05-16 23:38:18 +0300

    Implementing the observer - added PublisherInterface

---
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/PublisherInterface.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.src.DomainLayer.Store;
+using eCommerce.src.DomainLayer.User.Roles;
+using eCommerce.src.ServiceLayer.ResultService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.DomainLayer
+{
+    public interface PublisherInterface
+    {
+        Result<Boolean> notifyStorePurchase(Product product, int quantity);
+        Result<Boolean> notifyStoreClosed();
+        Result<Boolean> notifyStoreOpened();
+        Result<Boolean> notifyOwnerSubscriptionRemoved(String ownerID, StoreOwner removedOwner);
+    }
+}
