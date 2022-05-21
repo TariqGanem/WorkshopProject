bc34975c49bf7d566c081f1df747580ea2aa3aec
yazan <yazan.ahmad.abass@gmail.com>
2022-05-21 21:16:30 +0300

    Added Logical Polices for BuyNow

---
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/Stores/Purchase/Policies/LogicPolicy.cs
@@ -0,0 +1,51 @@
+﻿using eCommerce.src.DomainLayer.Store;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.DomainLayer.Stores.Purchase.Policies
+{
+    internal abstract class LogicPolicy
+    {
+        public string Id { get; }
+        public List<LogicPolicy> Policies { get; }
+
+        public abstract bool IsSatisfiedCond(ConcurrentDictionary<Product, int> bag, User.User user);
+
+        protected LogicPolicy(string id = "")
+        {
+            this.Id = id;
+            if (id.Equals(""))
+                this.Id = Service.GenerateId();
+            this.Policies = new List<LogicPolicy>();
+        }
+        protected LogicPolicy(List<LogicPolicy> policies, string id = "") : this(id)
+        {
+            if (policies != null)
+                this.Policies = policies;
+        }
+        public bool AddPolicy(LogicPolicy policy)
+        {
+            LogicPolicy p = Policies.Find(p => p.Id.Equals(policy.Id));
+            if (p != null)
+            {
+                throw new Exception("Policy already exists, therefore can't be added!");
+            }
+
+            Policies.Add(policy);
+            return true;
+        }
+
+        public LogicPolicy RemovePolicy(string id)
+        {
+            LogicPolicy policy = Policies.Find(policy => policy.Id.Equals(id));
+            if (policy != null)
+            {
+                Policies.Remove(policy);
+                return policy;
+            }
+            throw new Exception("Policy not found, therefore can't be removed!");
+        }
+    }
+}
---
+++ Version-1/eCommerce/eCommerce/src/DomainLayer/Stores/Purchase/Types/BuyNow.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.src.DomainLayer.Store;
+using eCommerce.src.DomainLayer.Stores.Purchase.Policies;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.DomainLayer.Stores.Purchase.Types
+{
+    internal class BuyNow : IPurchaseType
+    {
+        public string Id => throw new NotImplementedException();
+
+        public bool AddPolicy(LogicPolicy policy, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditPolicy(Dictionary<string, object> info, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSatisfiedCond(ConcurrentDictionary<Product, int> bag, User.User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogicPolicy RemovePolicy(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
