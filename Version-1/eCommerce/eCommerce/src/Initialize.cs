05fe8dcdd9bdf9f89b38367471da6837a40b2652
eran <mando@post.bgu.ac.il>
2022-06-07 02:09:05 +0300

    added Initializer file

--- Version-1/eCommerce/eCommerce/src/Initialize.cs
+++ Version-1/eCommerce/eCommerce/src/Initialize.cs
@@ -11,13 +11,12 @@ namespace eCommerce.src
     {
         public static void Initializer()
         {
-            //String adminId = "e035ddf3301245119e39a6f2a81143da";
             DBUtil dbutil;
             String connection_url = "mongodb+srv://Workshop:Workshop@workshopproject.frdmk.mongodb.net/?retryWrites=true&w=majority";
             String db_name = "Scenario1";
             dbutil = DBUtil.getInstance(connection_url, db_name); // db initializer
-            String extSystemURL = "https://cs-bgu-wsep.herokuapp.com/";  
             Proxy.getInstance(); // external system init - MOCK
+            //String extSystemURL = "https://cs-bgu-wsep.herokuapp.com/";
             // Proxy.getInstance(extSystemURL); // external system init
         }
     }
