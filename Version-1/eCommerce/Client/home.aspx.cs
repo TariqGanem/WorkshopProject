c3c658813d6c5631d6db50bd10c65cb738cef7c7
mas98ahmed <ahmedmas@post.bgu.ac.il>
2022-06-06 23:17:22 +0300

    fix problems

--- Version-1/eCommerce/Client/home.aspx.cs
+++ Version-1/eCommerce/Client/home.aspx.cs
@@ -14,48 +14,53 @@ namespace Client
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int counter = 0;
-            while (counter < 30)
-            {
-                try
-                {
-                    if (Request.QueryString["keyword"] != null)
-                    {
-                        ShopHandler a = new ShopHandler();
-                        DataListproducts.DataSource = a.search(Request.QueryString["keyword"].ToString());
-                        DataListproducts.DataBind();
-                    }
-                    else
-                    {
-                        ShopHandler a = new ShopHandler();
-                        DataListproducts.DataSource = a.getAllProducts();
-                        DataListproducts.DataBind();
-                    }
-                    return;
-                }
-                catch
-                {
-                    Thread.Sleep(1000);
-                    counter++;
-                }
-            }
-            if(counter >= 10)
-                throw new Exception("server not responding");
+            //int counter = 0;
+            //while (counter < 30)
+            //{
+            //    try
+            //    {
+            //        if (Request.QueryString["keyword"] != null)
+            //        {
+            //            ShopHandler a = new ShopHandler();
+            //            DataListproducts.DataSource = a.search(Request.QueryString["keyword"].ToString());
+            //            DataListproducts.DataBind();
+            //        }
+            //        else
+            //        {
+            //            ShopHandler a = new ShopHandler();
+            //            DataListproducts.DataSource = a.getAllProducts();
+            //            DataListproducts.DataBind();
+            //        }
+            //        return;
+            //    }
+            //    catch
+            //    {
+            //        Thread.Sleep(1000);
+            //        counter++;
+            //    }
+            //}
+            //if(counter >= 10)
+            //    throw new Exception("server not responding");
         }
 
         protected void DataListproducts_ItemCommand1(object source, DataListCommandEventArgs e)
         {
 
-            if (e.CommandName == "add_to_cart")
-            {
-                string[] cargs = e.CommandArgument.ToString().Split(',');
-                Session["Id"] = cargs[0];
-                Session["storeId"] = cargs[1];
-                Session["Name"] = cargs[2];
-                Session["Price"] = cargs[3];
-                Session["Catagory"] = cargs[4];
-                Response.Redirect("~/Product.aspx");
-            }
+            //if (e.CommandName == "add_to_cart")
+            //{
+            //    string[] cargs = e.CommandArgument.ToString().Split(',');
+            //    Session["Id"] = cargs[0];
+            //    Session["storeId"] = cargs[1];
+            //    Session["Name"] = cargs[2];
+            //    Session["Price"] = cargs[3];
+            //    Session["Catagory"] = cargs[4];
+            //    Response.Redirect("~/Product.aspx");
+            //}
+        }
+
+        protected void DataListproducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
