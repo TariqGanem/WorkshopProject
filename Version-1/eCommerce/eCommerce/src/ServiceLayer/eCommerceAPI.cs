5bce6f9e2eaff2975d137c9579c19f894d1188a6
eran <mando@post.bgu.ac.il>
2022-06-09 00:37:12 +0300

    System Initializes here + redoing the functionality

--- Version-1/eCommerce/eCommerce/src/ServiceLayer/eCommerceAPI.cs
+++ Version-1/eCommerce/eCommerce/src/ServiceLayer/eCommerceAPI.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using eCommerce.src.DataAccessLayer;
 using eCommerce.src.DomainLayer;
 using eCommerce.src.DomainLayer.User;
+using eCommerce.src.ExternalSystems;
 using eCommerce.src.ServiceLayer;
 using eCommerce.src.ServiceLayer.Controllers;
 using eCommerce.src.ServiceLayer.Objects;
 using eCommerce.src.ServiceLayer.ResultService;
+using Newtonsoft.Json;
 
 namespace eCommerce.src.ServiceLayer
 {
 
     public interface IeCommerceAPI : IUserController, IGuestController, IRegisteredUserController, ISystemAdminController, IStoreStaffController { }
-    public class eCommerceSystem : IeCommerceAPI
+    public class eCommerceSystem 
     {
         public IUserController UserController { get; set; }
         public IGuestController GuestController { get; set; }
@@ -20,9 +24,32 @@ namespace eCommerce.src.ServiceLayer
         public SystemAdminController SystemAdminController { get; set; }
         public IStoreStaffController StoreStaffController { get; set; }
         public SystemFacade systemFacade { get; set; }
+        public NotificationsService notificationsService { get; set; }
 
-        public eCommerceSystem()
+        public eCommerceSystem(String config_path = @"..\eCommerce\Config.json" , string configData = "")
         {
+            Config config;
+            if (!(configData.Equals(String.Empty)))
+            {
+                config = JsonConvert.DeserializeObject<Config>(configData);
+
+            }
+            else
+            {
+                config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(config_path));
+
+            }
+
+            //validate JSON
+            if ((config.externalSystem_url is null) || (config.mongoDB_url is null)
+                || (config.password is null) || (config.email is null) || (config.dbenv is null))
+            {
+                Logger.GetInstance().LogError("Invalid JSON format - One or more missing attribute has been found in the config JSON");
+                Environment.Exit(1);
+            }
+
+            DBUtil.getInstance(config.mongoDB_url, config.dbenv);
+            Proxy.getInstance(config.externalSystem_url);
             systemFacade = new SystemFacade();
             UserController = new UserController(systemFacade);
             GuestController = new GuestController(systemFacade);
@@ -31,138 +58,5 @@ namespace eCommerce.src.ServiceLayer
             StoreStaffController = new StoreStaffController(systemFacade);
         }
 
-        #region User Related Methods
-        public Result AddProductToCart(string userId, string productId, int quantity, string storeId)
-        {
-            return UserController.AddProductToCart(userId, productId, quantity, storeId);
-        }
-
-        public Result<RegisteredUserSO> AddSystemAdmin(string sysAdminId, string userName)
-        {
-            return SystemAdminController.AddSystemAdmin(sysAdminId, userName);
-        }
-
-        public Result<double> GetTotalShoppingCartPrice(string userId)
-        {
-            return UserController.GetTotalShoppingCartPrice(userId);
-        }
-
-        public Result<UserHistorySO> GetUserPurchaseHistory(string userId)
-        {
-            return RegisteredUserController.GetUserPurchaseHistory(userId);
-        }
-
-        public Result<ShoppingCartSO> GetUserShoppingCart(string userId)
-        {
-            return UserController.GetUserShoppingCart(userId);
-        }
-
-        public Result<GuestUserSO> Login()
-        {
-            return GuestController.Login();
-        }
-
-        public Result<RegisteredUserSO> Login(string userName, string password)
-        {
-            return RegisteredUserController.Login(userName, password);
-        }
-
-        public Result Logout(string userId)
-        {
-            return UserController.Logout(userId);
-        }
-
-        public Result<ShoppingCartSO> Purchase(string userId, IDictionary<string, object> paymentDetails, IDictionary<string, object> deliveryDetails)
-        {
-            return UserController.Purchase(userId, paymentDetails, deliveryDetails);
-        }
-
-        public Result<RegisteredUserSO> Register(string username, string password)
-        {
-            return GuestController.Register(username, password);
-        }
-
-        public Result<RegisteredUserSO> RemoveSystemAdmin(string sysAdminId, string userName)
-        {
-            return SystemAdminController.RemoveSystemAdmin(sysAdminId, userName);
-        }
-
-        public Result UpdateShoppingCart(string userId, string storeId, string productId, int quantity)
-        {
-            return UserController.UpdateShoppingCart(userId, storeId, productId, quantity);
-        }
-
-        Result<UserHistorySO> ISystemAdminController.GetUserPurchaseHistory(string sysAdminId, string userId)
-        {
-            return SystemAdminController.GetUserPurchaseHistory(sysAdminId, userId);
-        }
-        #endregion
-
-        #region Store Related Methods
-        public Result AddStoreOwner(String addedOwnerID, String currentlyOwnerID, String storeID)
-        {
-            return StoreStaffController.AddStoreOwner(addedOwnerID, currentlyOwnerID, storeID);
-        }
-        public Result AddStoreManager(String addedManagerID, String currentlyOwnerID, String storeID)
-        {
-            return StoreStaffController.AddStoreManager(addedManagerID, currentlyOwnerID, storeID);
-        }
-        public Result RemoveStoreManager(String removedManagerID, String currentlyOwnerID, String storeID)
-        {
-            return StoreStaffController.RemoveStoreManager(removedManagerID, currentlyOwnerID, storeID);
-        }
-        public Result RemoveStoreOwner(String removedOwnerID, String currentlyOwnerID, String storeID)
-        {
-            return StoreStaffController.RemoveStoreOwner(removedOwnerID, currentlyOwnerID, storeID);
-        }
-        public Result SetPermissions(String storeID, String managerID, String ownerID, LinkedList<int> permissions)
-        {
-            return StoreStaffController.SetPermissions(storeID, managerID, ownerID, permissions);
-        }
-        public Result RemovePermissions(String storeID, String managerID, String ownerID, LinkedList<int> permissions)
-        {
-            return StoreStaffController.RemovePermissions(storeID, managerID, ownerID, permissions);
-        }
-        public Result<Dictionary<IStaffService, PermissionService>> GetStoreStaff(String ownerID, String storeID)
-        {
-            return StoreStaffController.GetStoreStaff(ownerID, storeID);
-        }
-        public Result<String> AddProductToStore(String userID, String storeID, String productName, double price, int initialQuantity, String category, LinkedList<String> keywords = null)
-        {
-            return StoreStaffController.AddProductToStore(userID, storeID, productName, price, initialQuantity, category, keywords);
-        }
-        public Result RemoveProductFromStore(String userID, String storeID, String productID)
-        {
-            return StoreStaffController.RemoveProductFromStore(userID, storeID, productID);
-        }
-        public Result EditProductDetails(String userID, String storeID, String productID, IDictionary<String, Object> details)
-        {
-            return StoreStaffController.EditProductDetails(userID, storeID, productID, details);
-        }
-        public Result<List<ProductService>> SearchProduct(IDictionary<String, Object> productDetails)
-        {
-            return StoreStaffController.SearchProduct(productDetails);
-        }
-        public Result<UserHistorySO> GetStorePurchaseHistory(String ownerID, String storeID, Boolean isSystemAdmin = false)
-        {
-            return !isSystemAdmin ? StoreStaffController.GetStorePurchaseHistory(ownerID, storeID) :
-                                SystemAdminController.GetStorePurchaseHistory(ownerID, storeID);
-        }
-
-        public Result<StoreService> OpenNewStore(string storeName, string userId)
-        {
-            return RegisteredUserController.OpenNewStore(storeName, userId);
-        }
-
-        public Result CloseStore(string userId, string storeId)
-        {
-            return RegisteredUserController.CloseStore(userId, storeId);
-        }
-
-        public void run()
-        {
-            //TODO
-        }
-        #endregion
     }
 }
