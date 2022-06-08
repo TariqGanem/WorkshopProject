0fd53347863816b63c4b338432acd95e4d72a24f
eran <mando@post.bgu.ac.il>
2022-06-08 23:09:40 +0300

    db robustness

--- Version-1/eCommerce/eCommerce/src/DataAccessLayer/DAO.cs
+++ Version-1/eCommerce/eCommerce/src/DataAccessLayer/DAO.cs
@@ -10,80 +10,164 @@ namespace eCommerce.src.DataAccessLayer
 {
     public class DAO<T>
     {
-        public IMongoCollection<BsonDocument> Documents { get; set; }
+        public IMongoCollection<BsonDocument> collection;
 
-        public DAO(IMongoDatabase db , string dbName)
+        //Constructor
+        public DAO(IMongoDatabase database, string collectinName)
         {
-            Documents = db.GetCollection<BsonDocument>(dbName);
+            collection = database.GetCollection<BsonDocument>(collectinName);
         }
-        public void Create(T dto)
+
+        public void Create(T dto, MongoDB.Driver.IClientSessionHandle session = null)
         {
             try
             {
                 var doc = dto.ToBsonDocument();
-                Documents.InsertOne(doc);
+                if (session != null)
+                {
+                    collection.InsertOne(session, doc);
+                }
+                else
+                {
+                    collection.InsertOne(doc);
+                }
+                DBUtil.connectionStatus = ConnectionStatus.OK;
             }
             catch (MongoWriteException e)
             {
-                Console.WriteLine(e.ToString());
-                Logger.GetInstance().LogError(e.ToString());
+                if (!e.ToString().Contains("E11000"))
+                {
+                    Console.WriteLine(e.ToString());
+                    Logger.GetInstance().LogError(e.ToString());
+                    updateConnectiviyError();
+                }
+
             }
             catch (Exception e)
             {
                 Logger.GetInstance().LogError(e.ToString());
+                updateConnectiviyError();
             }
         }
 
-        public T Delete(FilterDefinition<BsonDocument> filter)
+        public T Delete(FilterDefinition<BsonDocument> filter, MongoDB.Driver.IClientSessionHandle session = null)
         {
             try
             {
-                BsonDocument deletedDocument = Documents.FindOneAndDelete(filter);
+                BsonDocument deletedDocument;
+                if (session != null)
+                {
+                    //collection.DeleteOne(mapper.session , filter);
+                    deletedDocument = collection.FindOneAndDelete(session, filter);
+                }
+                else
+                {
+                    //collection.DeleteOne(filter);
+                    deletedDocument = collection.FindOneAndDelete(filter);
+                }
                 T dto = JsonConvert.DeserializeObject<T>(deletedDocument.ToJson());
+                DBUtil.connectionStatus = ConnectionStatus.OK;
                 return dto;
             }
             catch (MongoWriteException e)
             {
                 Console.WriteLine(e.ToString());
                 Logger.GetInstance().LogError(e.ToString());
+                updateConnectiviyError();
                 return default(T);
             }
             catch (Exception e)
             {
                 Logger.GetInstance().LogError(e.ToString());
+                updateConnectiviyError();
                 return default(T);
             }
         }
 
-        public T Load(FilterDefinition<BsonDocument> filter)
+        public T Load(FilterDefinition<BsonDocument> filter, MongoDB.Driver.IClientSessionHandle session = null)
         {
             try
             {
-                var Document = Documents.Find(filter).FirstOrDefault();
+                BsonDocument Document;
+
+                if (session != null)
+                {
+                    Document = collection.Find(session, filter).FirstOrDefault();
+                }
+                else
+                {
+                    Document = collection.Find(filter).FirstOrDefault();
+                }
+                if (Document is null) { return default; }
                 var json = Document.ToJson();
                 if (json.StartsWith("{ \"_id\" : ObjectId(")) { json = "{" + json.Substring(47); }
                 T dto = JsonConvert.DeserializeObject<T>(json);
+                DBUtil.connectionStatus = ConnectionStatus.OK;
                 return dto;
             }
             catch (MongoWriteException e)
             {
                 Console.WriteLine(e.ToString());
                 Logger.GetInstance().LogError(e.ToString());
+                updateConnectiviyError();
                 return default(T);
 
             }
             catch (Exception e)
             {
                 Logger.GetInstance().LogError(e.ToString());
+                updateConnectiviyError();
                 return default(T);
 
             }
         }
-        public void Update(FilterDefinition<BsonDocument> filter, UpdateDefinition<BsonDocument> update, Boolean upsert = false)
+
+
+        public void Update(FilterDefinition<BsonDocument> filter, UpdateDefinition<BsonDocument> update, Boolean upsert = false, MongoDB.Driver.IClientSessionHandle session = null)
         {
-            if (upsert)
-                Documents.UpdateOne(filter, update, new UpdateOptions() { IsUpsert = upsert });
-            else Documents.UpdateOne(filter, update);
+            try
+            {
+                if (upsert)
+                {
+                    if (session != null)
+                    {
+                        collection.UpdateOne(session, filter, update, new UpdateOptions() { IsUpsert = upsert });
+                    }
+                    else
+                    {
+                        collection.UpdateOne(filter, update, new UpdateOptions() { IsUpsert = upsert });
+                    }
+
+                }
+                else
+                {
+                    if (session != null)
+                    {
+                        collection.UpdateOne(session, filter, update);
+                    }
+                    else
+                    {
+                        collection.UpdateOne(filter, update);
+                    }
+                }
+
+                DBUtil.connectionStatus = ConnectionStatus.OK;
+
+            }
+            catch (MongoWriteException e)
+            {
+                Console.WriteLine(e.ToString());
+                Logger.GetInstance().LogError(e.ToString());
+                updateConnectiviyError();
+            }
+        }
+
+        public void updateConnectiviyError()
+        {
+            if (!DBUtil.connectionStatus.Equals(ConnectionStatus.Error))
+            {
+                DBUtil.connectionStatus = ConnectionStatus.Error;
+            }
         }
     }
 }
