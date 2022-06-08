25c8ea6ea7db5a3ebdeca19cc1068b78d6b883b2
eran <mando@post.bgu.ac.il>
2022-06-08 23:10:12 +0300

    POLICY DTO FIX

--- Version-1/eCommerce/eCommerce/src/DataAccessLayer/DataTransferObjects/Stores/Policies/Purchase/DTO_MaxProductPolicy.cs
+++ Version-1/eCommerce/eCommerce/src/DataAccessLayer/DataTransferObjects/Stores/Policies/Purchase/DTO_MaxProductPolicy.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Terminal3.DomainLayer.StoresAndManagement.Stores;
-
 namespace eCommerce.src.DataAccessLayer.DataTransferObjects
 {
     [BsonIgnoreExtraElements]
