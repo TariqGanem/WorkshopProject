9c5c17448b7246e6698cd4347c76e30f3051c7fc
eran <mando@post.bgu.ac.il>
2022-06-14 00:16:01 +0300

    DataController to help with displaying data to the client

---
+++ Version-1/eCommerce/eCommerce/src/ServiceLayer/Controllers/DataController.cs
@@ -0,0 +1,46 @@
+﻿using eCommerce.src.DomainLayer;
+using eCommerce.src.ServiceLayer.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.src.ServiceLayer.Controllers
+{
+    public interface IDataController
+    {
+        List<StoreService> GetAllStoresToDisplay();
+        List<ProductService> GetAllProductByStoreIDToDisplay(string storeID);
+        Boolean[] GetPermission(string userID, string storeID);
+
+    }
+
+    public class DataController : IDataController
+    {
+        //Properties
+        public ISystemFacade systemfacade { get; }
+
+        //Constructor
+        public DataController(ISystemFacade storesAndManagementInterface)
+        {
+            this.systemfacade = storesAndManagementInterface;
+        }
+
+        #region Methods
+        public List<StoreService> GetAllStoresToDisplay()
+        {
+            return systemfacade.GetAllStoresToDisplay();
+        }
+        public List<ProductService> GetAllProductByStoreIDToDisplay(string storeID)
+        {
+            return systemfacade.GetAllProductByStoreIDToDisplay(storeID);
+        }
+
+        public Boolean[] GetPermission(string userID, string storeID)
+        {
+            return systemfacade.GetPermission(userID, storeID);
+        }
+
+
+        #endregion
+    }
+}
