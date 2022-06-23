2fb0e8aff645e059aebe2e67cf670a9ee1f2fd84
eran <mando@post.bgu.ac.il>
2022-06-24 00:07:46 +0300

    Offer Unit Tests

---
+++ Version-1/eCommerce/eCommerceTests/DomainLayerTests/StoresTests/PolicyManagerTests/Offer/StoreOffer.cs
@@ -0,0 +1,92 @@
+﻿using eCommerce.src.DomainLayer.Store;
+using eCommerce.src.DomainLayer.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using eCommerce.src.DomainLayer.Stores.Policies.Offer;
+using Xunit;
+
+namespace eCommerceTests.DomainLayerTests.StoresTests.PolicyManagerTests
+{
+    public class StoreOffer
+    {
+        public RegisteredUser Owner1 { get; }
+        public RegisteredUser Owner2 { get; }
+        public RegisteredUser RegisteredUser { get; }
+
+        public Store Store { get; }
+
+        public Product Product { get; }
+
+        public Offer Offer { get; }
+
+        public StoreOffer()
+        {
+            Owner1 = new RegisteredUser("owner1@offer", "pass1");
+            Owner2 = new RegisteredUser("owner2@offer", "pass2");
+            Store = new Store("store1", Owner1);
+            Store.AddStoreOwner(Owner2, Owner1.Id);
+            string prodid = Store.AddNewProduct(Owner1.Id, "product", 100, 100, "");
+            RegisteredUser = new RegisteredUser("reg@user", "pass3");
+            Offer = new Offer(RegisteredUser.Id, prodid, 10, 10, Store.Id);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void sendOfferToStoreTest()
+        {
+            Store.SendOfferToStore(Offer);
+            Assert.Single(Store.OfferManager.PendingOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void sendOfferResponseDeclineTest()
+        {
+            Store.SendOfferToStore(Offer);
+            Store.SendOfferResponseToUser(Owner1.Id, "wrong id", false, -1);
+            Store.SendOfferResponseToUser("wrong id", Offer.Id, false, -1);
+            Assert.Single(Store.OfferManager.PendingOffers);
+            Store.SendOfferResponseToUser(Owner1.Id, Offer.Id, false, -1);
+            Assert.Empty(Store.OfferManager.PendingOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void sendOfferResponseCounterOfferTest()
+        {
+            Store.SendOfferToStore(Offer);
+            Store.SendOfferResponseToUser(Owner1.Id, "wrong id", false, 2);
+            Store.SendOfferResponseToUser("wrong id", Offer.Id, false, 2);
+            Assert.Single(Store.OfferManager.PendingOffers);
+            Store.SendOfferResponseToUser(Owner1.Id, Offer.Id, false, 2);
+            Assert.Empty(Store.OfferManager.PendingOffers);
+            Assert.True(Offer.CounterOffer == 2);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void sendOfferResponseAcceptTest()
+        {
+            Store.SendOfferToStore(Offer);
+            Store.SendOfferResponseToUser(Owner1.Id, Offer.Id, true, -1);
+            Assert.Single(Store.OfferManager.PendingOffers);
+            Store.SendOfferResponseToUser(Owner2.Id, Offer.Id, true, -1);
+            Assert.Empty(Store.OfferManager.PendingOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void OfferGetDataTest()
+        {
+            Store.SendOfferToStore(Offer);
+            List<Dictionary<string, object>> result = Store.getStoreOffers();
+            Assert.Single(result);
+        }
+    }
+}
---
+++ Version-1/eCommerce/eCommerceTests/DomainLayerTests/StoresTests/PolicyManagerTests/Offer/UserOffer.cs
@@ -0,0 +1,100 @@
+﻿using eCommerce.src.DomainLayer.Stores.Policies.Offer;
+using eCommerce.src.DomainLayer.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace eCommerceTests.DomainLayerTests.StoresTests.PolicyManagerTests
+{
+    public class UserOffer
+    {
+        public RegisteredUser RegisteredUser { get; }
+
+        public UserOffer()
+        {
+            RegisteredUser = new RegisteredUser("reg@user", "pass3");
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void AddOfferTest()
+        {
+            Assert.Empty(RegisteredUser.PendingOffers);
+            Assert.Empty(RegisteredUser.AcceptedOffers);
+            Offer result = RegisteredUser.SendOfferToStore("storeID", "productID", 10, 20);
+            Assert.NotNull(result);
+            Assert.Equal("storeID", result.StoreID);
+            Assert.Equal("productID", result.ProductID);
+            Assert.True(10 == result.Amount);
+            Assert.True(20 == result.Price);
+            Assert.Single(RegisteredUser.PendingOffers);
+            Assert.Empty(RegisteredUser.AcceptedOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void RemoveOfferTest()
+        {
+            Offer Offer = RegisteredUser.SendOfferToStore("storeID", "productID", 10, 10);
+            RegisteredUser.RemovePendingOffer(Offer.Id);
+            Assert.Empty(RegisteredUser.PendingOffers);
+            Assert.Empty(RegisteredUser.AcceptedOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void AcceptOfferTest()
+        {
+            Offer Offer = RegisteredUser.SendOfferToStore("storeID", "productID", 10, 10);
+            RegisteredUser.AcceptOffer(Offer.Id);
+            Assert.Empty(RegisteredUser.PendingOffers);
+            Assert.Single(RegisteredUser.AcceptedOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void DeclineOfferTest()
+        {
+            Offer Offer = RegisteredUser.SendOfferToStore("storeID", "productID", 10, 10);
+            RegisteredUser.DeclineOffer(Offer.Id);
+            Assert.Empty(RegisteredUser.PendingOffers);
+            Assert.Empty(RegisteredUser.AcceptedOffers);
+        }
+
+
+        [Theory()]
+        [Trait("Category", "Unit")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void CounterOfferOfferTest(bool accept)
+        {
+            Offer Offer = RegisteredUser.SendOfferToStore("storeID", "productID", 10, 10);
+            Offer.CounterOffer = 20;
+            RegisteredUser.CounterOffer(Offer.Id);
+            Assert.Single(RegisteredUser.PendingOffers);
+            Assert.Empty(RegisteredUser.AcceptedOffers);
+
+            RegisteredUser.AnswerCounterOffer(Offer.Id, accept);
+            Assert.Empty(RegisteredUser.PendingOffers);
+            if (accept)
+                Assert.Single(RegisteredUser.AcceptedOffers);
+            else
+                Assert.Empty(RegisteredUser.AcceptedOffers);
+        }
+
+
+        [Fact()]
+        [Trait("Category", "Unit")]
+        public void OfferGetDataTest()
+        {
+            RegisteredUser.SendOfferToStore("storeID", "productID", 10, 10);
+            List<Dictionary<string, object>> result = RegisteredUser.getUserPendingOffers();
+            Assert.Single(result);
+        }
+    }
+}
